d3e7bb6172af2519eb08f459223ff49a31d608ba
SPEEDFUE <clkoutgamer@gmail.com>
2023-10-30 18:54:06 +0530

    Create (Area of Rectangle and Square)EXP3.java

---
+++ (Area of Rectangle and Square)EXP3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Calculate {
+    public static void main(String[] args) {
+    Scanner s=new Scanner (System.in);
+    Area o=new Area();
+    System.out.print("Enter Length of Rectangle: ");
+    int a=s.nextInt();
+    System.out.print("Enter Breadth of Rectangle: ");
+    int b=s.nextInt();
+    System.out.print("Enter Sides of Square: ");
+    int c=s.nextInt();
+    o.Rectangle(a,b);
+    o.Square(c);
+    }
+}
+
+class Area{
+    int area;
+void Rectangle(int l, int b){
+        area=l*b;
+        System.out.println("Area of Rectangle is "+area);
+    }
+void Square(int s){
+        area=s*s;
+        System.out.println("Area of Square is "+area);
+    }
+}
