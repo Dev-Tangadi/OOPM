edd6c276145c88d78db05e65f81404264405913c
SPEEDFUE <clkoutgamer@gmail.com>
2023-10-30 19:04:54 +0530

    Create (EVEN ODD)EXP10.java

---
+++ (EVEN ODD)EXP10.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+//Create custom exception OddException
+class OddException extends Exception {
+    int num;
+
+    // Constructor to get input number
+    public OddException(int num) {
+        num = num; // Assigning the local variable num to the instance variable num
+    }
+}
+
+// Create a class MyExceptionDemo
+class MyExceptionDemo {
+    // Write a static function to throw OddException
+    public static void OddNoException(int num) throws OddException {
+        if (num % 2 != 0) {
+            throw new OddException(num);
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = s.nextInt();
+        try {
+            // Invoke OddNoException() in try block
+            MyExceptionDemo.OddNoException(x);
+            // Calculate and print square if the number is even
+            int square = x * x;
+            System.out.println("Square of the number: " + square);
+        } catch (OddException e) {
+            // Catch and print the custom exception message
+            System.out.println("OddException: Input number must be even, but got odd number");
+        }
+    }
+}
